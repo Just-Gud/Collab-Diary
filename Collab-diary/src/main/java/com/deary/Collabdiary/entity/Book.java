92cd5bf6f93b1dc5134353f9849a4923e4341501
Just-Gud <just.gudai@gmail.com>
2023-02-20 17:28:52 +0200

    Implemented Lombok and used it to reformat the Book class

--- Collab-diary/src/main/java/com/deary/Collabdiary/entity/Book.java
+++ Collab-diary/src/main/java/com/deary/Collabdiary/entity/Book.java
@@ -4,17 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long bookId;
     private String name;
     private String description;
-
-    public Book() {
-    }
 
     @Override
     public String toString() {
@@ -25,33 +30,4 @@ public class Book {
                 '}';
     }
 
-    public Book(Long bookId, String name, String description) {
-        this.bookId = bookId;
-        this.name = name;
-        this.description = description;
-    }
-
-    public Long getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(Long bookId) {
-        this.bookId = bookId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
