1c9c2c216759e15a62d602b3f156298ad741ef94
Just-Gud <just.gudai@gmail.com>
2023-02-21 17:54:36 +0200

    Created welcome message in properties && added it to HomeController

--- Collab-diary/src/main/java/com/deary/Collabdiary/controller/HomeController.java
+++ Collab-diary/src/main/java/com/deary/Collabdiary/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.deary.Collabdiary.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class HomeController {
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String welcomeMessage(){
-        return "Welcome ppl";
+        return welcomeMessage;
     }
 }
